b1eeeade2db08ef664bff6b73d5a398bf7d51ea2
Rustam <kodzokoff2016@yandex.ru>
2024-01-14 01:27:46 +0300

    Первая фикстура

--- test_google_new.py
+++ test_google_new.py
@@ -1,11 +1,21 @@
+import pytest
 from selene import browser, be, have
 
-def test_google():
+
+@pytest.fixture(scope='function', autouse=True)
+def size_browser():
+    browser.config.window_width = 1200
+    browser.config.height = 1200
+    yield
+    browser.quit()
+
+
+def test_google(size_browser):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_size_browser():
+def test_size_browser(size_browser):
     browser.open('https://google.com')
     browser.element('[name="q"]').type(';lkkljsdfjlkj lfdjlshkjdfh fkjsghlkfjs').press_enter()
     browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
