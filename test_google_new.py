c8ddad7209a35917dadd77b0da54a10cc930607f
Rustam <kodzokoff2016@yandex.ru>
2024-01-14 08:11:16 +0300

    Добавление папки Idea в gitignore

--- test_google_new.py
+++ test_google_new.py
@@ -11,17 +11,17 @@ def size_browser():
     browser.quit()
 
 
-def test_google(size_browser):
+def test_google():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
-def test_size_browser(size_browser):
+def test_size_browser():
     browser.open('https://google.com')
     browser.element('[name="q"]').type(';lkkljsdfjlkj lfdjlshkjdfh fkjsghlkfjs').press_enter()
     browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
     print('По запросу ;lkkljsdfjlkj lfdjlshkjdfh fkjsghlkfjs ничего не найдено')
 
 
-test_google(size_browser)
-test_size_browser(size_browser)
+test_google()
+test_size_browser()
